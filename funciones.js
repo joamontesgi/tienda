2f7c408854193f7a41fb86957b96542fca21e650
joamontesgi <joamontesgi@unal.edu.co>
2025-05-05 20:21:44 -0500

    Creacio botones .js

---
+++ funciones.js
@@ -0,0 +1,12 @@
+function init(){
+    var productos = document.getElementById("productos");
+    productos= productos.innerHTML=
+    `
+    <div>
+        <label> Producto A </label>
+        <input min="1" type="number" name="" id="">
+        <button class="btn btn-primary" type="button">Agregar</button>
+    </div>`;
+    
+
+}
